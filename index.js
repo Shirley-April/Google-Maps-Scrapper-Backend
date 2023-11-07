3197b2eb3893668b02a8c658e399b8b4ed3ebc11
Shirley-April <shirleyapril23@gmail.com>
2023-11-07 16:42:20 +0300

    add cors lib and accept query search params

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+
 const app = express();
 
 import { scrapeGoogleMaps } from "./scrapper.js";
 
+app.use(cors());
+
 app.get("/", async (request, response) => {
+  const search = request.query.search;
   try {
-    const results = await scrapeGoogleMaps();
+    const results = await scrapeGoogleMaps(search);
     response.send(results);
   } catch {
     response.send({ message: "An error occured" });
