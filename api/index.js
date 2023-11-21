6c4ed7082a815f0ff20786058a13a5e6ebd5f35b
Shirley-April <shirleyapril23@gmail.com>
2023-11-21 11:45:37 +0300

    add configs for vercel deployment

---
+++ api/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+import { scrapeGoogleMaps } from "../scrapper.js";
+
+app.use(cors());
+
+app.get("/api", (request, response) => {
+    response.send("Home route!")
+})
+
+app.get("/api/scrape", async (request, response) => {
+  const search = request.query.search;
+  try {
+    const results = await scrapeGoogleMaps(search);
+    response.send(results);
+  } catch {
+    response.send({ message: "An error occured" });
+  }
+});
+
+app.listen(3005, function () {
+  console.log("The server has started on port 3005");
+});
